e784504b66c4d1925e2960ba9307b5b17021db1f
narapat0147 <69285922+narapat0147@users.noreply.github.com>
2021-02-02 11:35:04 +0700

    Add files via upload

---
+++ condition204.js
@@ -0,0 +1,18 @@
+const grade = 'D';
+
+switch(grade)
+{
+    case 'A':
+        console.log('Grade A');
+        break;
+    case 'B':
+        console.log('Grade B');
+        break;
+    case 'C':
+        console.log('Grade C');
+        break;
+    case 'D':
+        console.log('Grade D');
+    default:
+        console.log('Error')
+}
